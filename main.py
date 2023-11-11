d5fc9e058bf6e0e74697482be5401bb9046c0454
Deema Rain <deemarain@dhcp-892bfda7.ucd.ie>
2023-11-11 13:44:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Length = float(input("what is the length of the rectangle? "))
+width = float(input("what is the width of the rectangle? "))
+area = Length*width
+print("the area of a rectangle is: ")
+print(area)
+quit()
+
+
+
